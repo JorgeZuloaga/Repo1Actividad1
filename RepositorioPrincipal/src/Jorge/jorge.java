a416f7bbdf8d1e96036f59940d77b6ba21cd6cf7
JorgeZuloaga <103762224+JorgeZuloaga@users.noreply.github.com>
2023-02-03 18:39:09 +0100

    Add files via upload

---
+++ RepositorioPrincipal/src/Jorge/jorge.java
@@ -0,0 +1,10 @@
+package Jorge;
+
+public class jorge {
+
+	public  void imprimirNombre() {
+		// TODO Auto-generated method stub
+		String saludo = "Hola, soy Jorge!!";
+		System.out.println(saludo);
+	
+}}
---
+++ RepositorioPrincipal/src/Main/main_activity.java
@@ -0,0 +1,14 @@
+package Main;
+
+import Jorge.jorge;
+
+public class main_activity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		jorge jorge = new jorge();
+		jorge.imprimirNombre();
+	}
+
+}
