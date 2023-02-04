3f9697b80805d8f22a91507c35f4bd1b55023358
Jose Ignacio Vazquez <cionagi@gmail.com>
2023-02-04 17:53:17 +0100

    añadir hobbies

--- RepositorioPrincipal/src/Ignacio/Ignacio.java
+++ RepositorioPrincipal/src/Ignacio/Ignacio.java
@@ -3,7 +3,8 @@ package Ignacio;
 public class Ignacio {
 
 	public void imprimirNombre() {
-		String saludo = "Hola, soy Ignacio";
+		String saludo = "Hola, soy Ignacio. Mis hobbys preferidos: escuchar musica, leer novelas y caminar.";
 		System.out.println(saludo);
+		
 	}
 }
