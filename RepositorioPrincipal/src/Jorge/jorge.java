031d1a99a0e9b8aa5f68d4e125d934e6f81ecd31
JorgeZuloaga <jorgezulogon@gmail.com>
2023-02-04 17:35:56 +0100

    hola

--- RepositorioPrincipal/src/Jorge/jorge.java
+++ RepositorioPrincipal/src/Jorge/jorge.java
@@ -4,7 +4,7 @@ public class jorge {
 
 	public  void imprimirNombre() {
 		// TODO Auto-generated method stub
-		String saludo = "Hola, soy Jorge!!";
+		String saludo = "Hola, soy Jorge!! Me gusta: correr, saltar y comer";
 		System.out.println(saludo);
 	
 }}
