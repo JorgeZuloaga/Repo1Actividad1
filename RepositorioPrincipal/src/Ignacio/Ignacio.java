5652fb6296bbf08afbdcb98d8509e4c47cbaaa8a
JorgeZuloaga <103762224+JorgeZuloaga@users.noreply.github.com>
2023-02-04 18:10:42 +0100

    Update Ignacio.java

--- RepositorioPrincipal/src/Ignacio/Ignacio.java
+++ RepositorioPrincipal/src/Ignacio/Ignacio.java
@@ -3,7 +3,7 @@ package Ignacio;
 public class Ignacio {
 
 	public void imprimirNombre() {
-		String saludo = "Hola, soy Ignacio";
+		String saludo = "Hola, soy Ignacio, me gusta la música, leer novelas y caminar por el campo";
 		System.out.println(saludo);
 	}
 }
