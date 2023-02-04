9ae1032a7c8659f9c203a820875181401eb59ce1
JorgeZuloaga <jorgezulogon@gmail.com>
2023-02-04 18:54:19 +0100

    comida favorita

--- RepositorioPrincipal/src/Jorge/jorge.java
+++ RepositorioPrincipal/src/Jorge/jorge.java
@@ -4,7 +4,7 @@ public class jorge {
 
 	public  void imprimirNombre() {
 		// TODO Auto-generated method stub
-		String saludo = "Hola, soy Jorge!! Me gusta: correr, saltar y comer";
+		String saludo = "Hola, soy Jorge!! Me gusta: correr, saltar y comer. Además mis platos favoritos son: tortilla, sopa y merluza";
 		System.out.println(saludo);
 	
 }}
